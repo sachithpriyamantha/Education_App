f267b14a460a2b82ceb9905f195259636cddd5de
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-09-02 21:53:18 +0530

    update

---
+++ lib/welcomeScrean.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class welcomeScrean extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Stack(children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 1.6,
+                decoration: const BoxDecoration(
+                    color:Colors.white,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 1.6,
+                decoration: const BoxDecoration(
+                    color: Color(0xFF674AEF),
+                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(70)),
+                    
+              ),
+              ),
+            ],),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
