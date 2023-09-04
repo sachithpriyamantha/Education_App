8ca7c6c7b15b2b859dafcb165c45d60536cb6ac8
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-09-04 10:52:08 +0530

    update

--- lib/welcomeScrean.dart
+++ lib/welcomeScrean.dart
@@ -65,7 +65,42 @@ class welcomeScrean extends StatelessWidget{
                       wordSpacing: 2,
                     ),
                   ),
-                ],),
+                  SizedBox(height: 15,),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Text(
+                        "Learning with Pleasure with Dear Programer, Wherever you are.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black.withOpacity(0.6),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  Material(
+                    color: Color(0xFF674AEF),
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: (){},
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 80
+                        ),
+                        child: Text(
+                          "Get Start",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+                ),
               ),
             ),
           ],
