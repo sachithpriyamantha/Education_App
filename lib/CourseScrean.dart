5729a127c87c1ba7fab5853cb01f887425b603d4
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-09-07 21:42:57 +0530

    upd

--- lib/CourseScrean.dart
+++ lib/CourseScrean.dart
@@ -19,13 +19,13 @@ class _CourseScreanState extends State<CourseScrean> {
         centerTitle: true,
         title: Text(
             widget.img,
-          style: TextStyle(
+          style: const TextStyle(
             fontWeight: FontWeight.bold,
             letterSpacing: 1,
           ),
 
         ),
-        actions: [
+        actions: const [
           Padding(
             padding: EdgeInsets.only(right: 10),
             child: Icon(
@@ -37,28 +37,28 @@ class _CourseScreanState extends State<CourseScrean> {
         ],
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         child: ListView(
              children: [
                Container(
-                 padding: EdgeInsets.all(5),
+                 padding: const EdgeInsets.all(5),
                  width: MediaQuery.of(context).size.width,
                  height: 200,
                  decoration: BoxDecoration(
                    borderRadius: BorderRadius.circular(20),
-                   color: Color(0xFFF5F3FF),
+                   color: const Color(0xFFF5F3FF),
                    image: DecorationImage(
                      image: AssetImage("images/${widget.img}.png"),
                    ),
                  ),
                  child: Center(
                    child: Container(
-                     padding: EdgeInsets.all(5),
-                     decoration: BoxDecoration(
+                     padding: const EdgeInsets.all(5),
+                     decoration: const BoxDecoration(
                        color: Colors.white,
                        shape: BoxShape.circle,
                      ),
-                     child: Icon(
+                     child: const Icon(
                        Icons.play_arrow_rounded,
                      color: Color(0xFF674AEF),
                        size: 40,
@@ -66,7 +66,63 @@ class _CourseScreanState extends State<CourseScrean> {
                    ),
                  ),
                ),
-               
+               const SizedBox(height: 15),
+               Text("${widget.img} Complete Course",
+               style: const TextStyle(
+                 fontSize: 22,
+                 fontWeight: FontWeight.w600,
+
+               ),
+               ),
+               const SizedBox(height: 5),
+               Text(
+                 "CREATED BY SACHITH PRIYAMANTHA",
+                 style: TextStyle(
+                   fontSize: 16,
+                   fontWeight: FontWeight.w500,
+                   color: Colors.black.withOpacity(0.7),
+                 ),
+               ),
+               const SizedBox(height: 5),
+               Text(
+                 "5 Videos",
+                 style: TextStyle(
+                   fontSize: 15,
+                   fontWeight: FontWeight.w500,
+                   color: Colors.black.withOpacity(0.5),
+                 ),
+               ),
+               const SizedBox(height: 20),
+               Container(
+                 padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                 decoration: BoxDecoration(
+                   color: const Color(0xFFF5F3FF),
+                   borderRadius: BorderRadius.circular(10),
+                 ),
+                 child: Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                   children: [
+                     Material(
+                       color: const Color(0xFF674AEF),
+                       borderRadius: BorderRadius.circular(10),
+                       child: InkWell(
+                         onTap: (){},
+                         child: Container(
+                           padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                           child: const Text(
+                             "videos",
+                             style: TextStyle(
+                               color: Colors.white,
+                               fontSize: 20,
+                               fontWeight: FontWeight.w500,
+                             ),
+                           ),
+                         ),
+                       ),
+                     )
+                   ],
+                 ),
+               )
              ],
         ),
       ),
