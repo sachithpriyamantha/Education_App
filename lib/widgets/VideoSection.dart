02d59d2a782acd5578a6395a3bd97f71acc4d982
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-09-11 11:24:22 +0530

    update

--- lib/widgets/VideoSection.dart
+++ lib/widgets/VideoSection.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 
 class VideoSection extends StatelessWidget{
 
-  ListView videoList = [
+  List videoList = [
     'Introduction to Flutter',
     'Installing Flutter on Windows',
-  ] as ListView;
+    'Creating Our First App',
+  ];
 
   @override
   Widget build(BuildContext context){
-    return ListView.builder();
+    return ListView.builder(
+      itemCount: videoList.length,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index){
+        return ListTile(
+          leading: Container(
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: index == 0 ?
+                const Color(0xFF674AEF) :
+                const Color(0xFF674AEF).withOpacity(0.6),
+              ),
+          ),
+        );
+      },
+    );
   }
 }
