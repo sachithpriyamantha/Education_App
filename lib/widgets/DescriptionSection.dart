1ea29bd6abe1845a2c4bf2ba547ec081a83b275d
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-09-12 10:36:43 +0530

    update

--- lib/widgets/DescriptionSection.dart
+++ lib/widgets/DescriptionSection.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class DescriptionSection extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Column();
+    return Padding(
+        padding: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text("vjkakjv bvjdabjvkjv jvbkvbkf v vjv kv  j beibf v bf nk kn h efbfe jbij beginfnjgifj ",
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.black.withOpacity(0.7),
+          ),
+            textAlign: TextAlign.justify,
+          ),
+        ],
+      ),
+    );
   }
 }
